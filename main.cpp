fc686dced0cd5aec5221644c5207fc1e49e3cb2b
yool kneverno <z.dummy.number.2@gmail.com>
2025-08-25 15:50:01 -0500

    i'm commiting the changes, hopefully it fixes the build issue

--- main.cpp
+++ main.cpp
@@ -5,11 +5,13 @@
 using namespace std;
 
 unsigned int hilbert(int x, int y) {
+	//this function is dificult to explain without diagrams,
 	short orientation = 0;
 	int reflectedX = 0;
 	int reflectedY = 0;
 	unsigned int result = 0;
 
+	//for the nth bit of x and y(bitwise opperations used)
 	for (int n = 15; n >= 0; n--) {
 		if (orientation == 0) {
 			reflectedX |= ((x >> n) & 1) << n;
@@ -30,10 +32,15 @@ unsigned int hilbert(int x, int y) {
 		else {
 			return 15;
 		}
+
+
 		result |= ((((reflectedX >> n) & 1) ^ ((reflectedY >> n) & 1)) | (((reflectedX >> n) & 1) << 1)) << (2 * n);
-		if (((reflectedY >> n) & 1) == 0) {
-			orientation ^= ((reflectedX >> n) & 1) + 1;
+
+		if (reflectedY >> n) &1) == 0) {
+			orientation ^= (((reflectedX >> n) & 1) + 1);
 		}
+		
+		
 	}
 
 
@@ -42,8 +49,8 @@ unsigned int hilbert(int x, int y) {
 }
 
 int main() {
-	char user_input;
 
+	//main input
 	cout << hilbert(3, 2) << "this is indeed a change, fuck you it did'nt show up, damn it.";
-	return 1;
+	return 0;
 }
